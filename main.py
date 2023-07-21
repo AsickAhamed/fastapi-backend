d8db9884c874f41f26b99f50f17f874d999a536c
Ahamed, Asick (DBAN) <asick.ahamed@smithsdetection.com>
2023-07-21 10:22:42 +0530

    shared env file configured

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.utils.generate_config import app_config
-
+from src.utils.logger import Logger
 if __name__ == "__main__":
+    log_helper = Logger()
+    log_helper.debug("application started")
+    log_helper.info("log from info")
+    log_helper.error("log from error")
     print("hello world")
     print(app_config["HOST"])
     print(app_config["DB"])
+
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,17 +1,33 @@
 import logging
+from src.utils.generate_config import app_config
 
 
-class logger:
+class Logger:
 
     def __init__(self):
-        self.logger = logging.getLogger("watchbird")
-        self.handler = logging.FileHandler("logs/watchbird.log")
-        self.formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        self.handler.setFormatter(self.formatter)
-        self.logger.addHandler(self.handler)
 
-        self.error_logger = logging.getLogger("watchbird_error")
-        self.error_handler = logging.FileHandler("logs/watchbird_error.log")
-        self.error_handler.setFormatter(self.formatter)
-        self.error_logger.addHandler(self.error_handler)
+        self._log_name = app_config["APP_NAME"]
+        self._log_dir = app_config["LOG_DIR"]
 
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+        self.__debug_log = logging.getLogger(self._log_name + "_debug")
+        self.__debug_log.setLevel(logging.DEBUG)
+        debug_handler = logging.FileHandler(self._log_dir + "/" + self._log_name + "_debug.log")
+        debug_handler.setFormatter(formatter)
+        self.__debug_log.addHandler(debug_handler)
+        self.debug = self.__debug_log.debug
+
+        self.__info_log = logging.getLogger(self._log_name + "_info")
+        self.__info_log.setLevel(logging.INFO)
+        info_handler = logging.FileHandler(self._log_dir + "/" + self._log_name + "_info.log")
+        info_handler.setFormatter(formatter)
+        self.__info_log.addHandler(info_handler)
+        self.info = self.__info_log.info
+
+        self.__error_log = logging.getLogger(self._log_name + "_error")
+        self.__error_log.setLevel(logging.ERROR)
+        error_handler = logging.FileHandler(self._log_dir + "/" + self._log_name + "_error.log")
+        error_handler.setFormatter(formatter)
+        self.__error_log.addHandler(error_handler)
+        self.error = self.__error_log.error
